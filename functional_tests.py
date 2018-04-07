c42fc12c276793b9c803a47dbc07be86edb0fa30
Thomas Slaymaker <tslaymaker@progrexion.com>
2018-04-06 18:29:52 -0600

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
         # He is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placehlder'),
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
@@ -38,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting him to add another time. He enters "Use peacock feathers to make a fly"
