9e3e22f0b5f701cce8cfdd646d620ffc2efd61aa
Thomas Slaymaker <tslaymaker@progrexion.com>
2018-04-11 20:48:54 -0600

    refactored the duplicated code with a helper method

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Andy heard about a web to-do app. He checks out the home page
         self.browser.get('http://localhost:8000')
@@ -34,24 +39,19 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(5)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.browser.implicitly_wait(8)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting him to add another time. He enters "Use peacock feathers to make a fly"
         # Andy is very methodical
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(5)
+        self.browser.implicitly_wait(8)
 
         # The page updates again, and now shows both items on his list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Andy wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him -- there is some
